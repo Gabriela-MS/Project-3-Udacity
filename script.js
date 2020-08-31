cb5c460561afbfe1baadd0a5081e80c83b3eb633
Gabriela-MS <70541622+Gabriela-MS@users.noreply.github.com>
2020-08-31 22:09:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//Final file of Pixel Art Maker
+
+
+function makeGrid() {
+//Better cleaning the canvas before anything
+  $('#pixelCanvas').children().remove();
+  
+//Receiving the values for height and width
+  var height = $("#input_height").val();
+  var width = $("#input_width").val();
+  
+//Receiving table
+  var table = $("#pixelCanvas");
+
+// Loop to build grid
+//Info on the row  (still "fuzzy") at http://index-of.es/JS/Powers%20Sh.%20-%20JavaScript.Cookbook%20-%202010.pdf
+// Remember: 'i' & 'j' are the generic "counting variables"
+    for (var i = 0; i < height; i++) {
+      $('#pixelCanvas').append(`<tr id='row${i}'></tr>`);
+      for (var j = 0; j < width; j++) {
+        $(`#row${i}`).append('<td></td>');
+      }
+     }
+  
+  table.on("click", "td", function() {
+//Definition of color (in the color picker)
+    var defColor = $("#colorPicker").val();
+//Coloring pixel on the table -> the background of the cells
+    $(this).attr("bgColor", defColor);
+   });
+} //Closing of function makeGrid - It's not "lost" :D
+
+//Prevent default value (defined in HTML) to make grid from start ofc
+ $("#button").on("click", function(evt) {
+    evt.preventDefault();
+    makeGrid();
+    return false;
+  });
